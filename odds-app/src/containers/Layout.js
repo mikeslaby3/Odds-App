d7b697d9839ed38fe120ea04245fd3c1dcec7940
Mike Slaby <mikeslaby3@gmail.com>
2019-10-09 13:49:54 -0700

    Restructured game component by odds market

--- odds-app/src/containers/Layout.js
+++ odds-app/src/containers/Layout.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Filter from '../components/Filter';
 import Button from '../components/Button';
 import Game from '../components/Game';
+import Header from "../components/Header";
 
 const API_KEY = process.env.REACT_APP_ODDS_API_KEY;
 
@@ -28,7 +29,6 @@ class Layout extends Component {
       .get(`https://api.the-odds-api.com/v3/sports/?apiKey=${API_KEY}`)
       .then(res => {
         this.setState({ sportsInSeason: res.data.data });
-        console.log(res.data.data);
       })
       .catch(err => {
         console.log(err);
@@ -51,9 +51,8 @@ class Layout extends Component {
     this.setState({ selectedMarket: e.target.value });
   };
 
-  createQueryUrl = () => {
+  createQueryUrl = (market) => {
     let sport = this.state.selectedSport
-    let market;
     switch (market) {
       case 'Moneyline':
         market = 'h2h';
@@ -66,20 +65,25 @@ class Layout extends Component {
         break;
       default: market = 'spreads';
     } 
-    // return `https://api.the-odds-api.com/v3/odds/?apiKey=${API_KEY}&sport=${sport}&mkt=${market}&region=us`;
-    return `https://api.the-odds-api.com/v3/odds/?apiKey=${API_KEY}&sport=${sport}&mkt=spreads&region=us`;
+    return `https://api.the-odds-api.com/v3/odds/?apiKey=${API_KEY}&sport=${sport}&mkt=${market}&region=us`;
   }
 
   handleSubmit = () => {
-    axios.get(this.createQueryUrl())
+    axios.get(this.createQueryUrl(this.state.selectedMarket))
       .then(res => {
         this.setState({ gameLines: res.data.data });
-        // console.log(this.state.selectedMarket);
-        console.log(res.data.data);
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       });
+  };
+
+  getTeamCity = team => {
+    const wordArray = team.split(" ");
+    const justTheCityArray =
+      wordArray.length === 3 ? wordArray.slice(0, 2) : wordArray.slice(0, 1);
+    const city = justTheCityArray.join(" ");
+    return city;
   };
 
   render() {
@@ -95,35 +99,76 @@ class Layout extends Component {
         ));
     }
 
-    const markets = this.createDropdownOptions(this.state.markets)
+    const markets = this.createDropdownOptions(this.state.markets);
 
     let games = <p>No games to show</p>;
 
     if (this.state.gameLines) {
       if (this.state.selectedMarket === 'Spread') {
-        games = this.state.gameLines.filter(game => game.sites[0]).map((game, index) => (
-          <Game
-            key={index}
-            awayTeam={game.teams[0]}
-            homeTeam={game.teams[1]}
-            time={game.commence_time}
-            market={this.state.selectedMarket}
-            awayOdds={game.sites[0].odds.spreads.odds[0]}
-            homeOdds={game.sites[0].odds.spreads.odds[1]}
-            awayPoints={game.sites[0].odds.spreads.points[0]}
-            homePoints={game.sites[0].odds.spreads.points[1]}
-          />
-        ));
+        games = this.state.gameLines
+          .filter(game => game.sites[0])
+          .map((game, index) => (
+            <Game
+              key={index}
+              teamOne={game.teams[0]}
+              teamTwo={game.teams[1]}
+              homeTeam={this.getTeamCity(game.home_team)}
+              time={game.commence_time}
+              market={this.state.selectedMarket}
+              oddsTitleOne={this.getTeamCity(game.teams[0])}
+              oddsTitleTwo={this.getTeamCity(game.teams[1])}
+              teamOneOdds={game.sites[0].odds.spreads.odds[0]}
+              teamTwoOdds={game.sites[0].odds.spreads.odds[1]}
+              teamOnePoints={game.sites[0].odds.spreads.points[0]}
+              teamTwoPoints={game.sites[0].odds.spreads.points[1]}
+            />
+          ));
+      } else if (this.state.selectedMarket === 'Moneyline') {
+        games = this.state.gameLines
+          .filter(game => game.sites[0])
+          .map((game, index) => (
+            <Game
+              key={index}
+              teamOne={game.teams[0]}
+              teamTwo={game.teams[1]}
+              homeTeam={this.getTeamCity(game.home_team)}
+              time={game.commence_time}
+              market={this.state.selectedMarket}
+              oddsTitleOne={this.getTeamCity(game.teams[0])}
+              oddsTitleTwo={this.getTeamCity(game.teams[1])}
+              teamOneOdds={game.sites[0].odds.h2h[0]}
+              teamTwoOdds={game.sites[0].odds.h2h[1]}
+            />
+          ));
+      } else if (this.state.selectedMarket === 'Over Under') {
+        games = this.state.gameLines
+          .filter(game => game.sites[0])
+          .map((game, index) => (
+            <Game
+              key={index}
+              teamOne={game.teams[0]}
+              teamTwo={game.teams[1]}
+              homeTeam={this.getTeamCity(game.home_team)}
+              time={game.commence_time}
+              market={this.state.selectedMarket}
+              oddsTitleOne="Over"
+              oddsTitleTwo="Under"
+              teamOneOdds={game.sites[0].odds.totals.odds[0]}
+              teamTwoOdds={game.sites[0].odds.totals.odds[1]}
+              teamOnePoints={game.sites[0].odds.totals.points[0]}
+              teamTwoPoints={game.sites[0].odds.totals.points[1]}
+            />
+          ));
       }
     }
 
     return (
       <Fragment>
         <StyledContainer>
+          <Header />
           <Filter
             title="NFL or NCAA"
             directions="Choose a Sport"
-            // value={props.selectedValue}
             change={this.handleSportChoice}
           >
             {sports}
